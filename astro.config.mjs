305274ba71e57a977e873005941645cefa7d1b19
mcshell7 <mishakhamardiuk@gmail.com>
2024-12-14 20:58:57 -0500

    Privacy policy updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://hamadev.net',
   integrations: [sitemap(), mdx({
     ...markdownConfig,
     extendPlugins: false
